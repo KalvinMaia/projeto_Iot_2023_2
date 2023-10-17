adea04f9554cc1a1c2746ffb91e3ec44b5ed8710
KalvinMaia <121024789+KalvinMaia@users.noreply.github.com>
2023-10-17 10:04:26 -0300

    Add files via upload

---
+++ teste_pratica_esp32-mqtt-adafruitIO.ino
@@ -0,0 +1,85 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <PubSubClient.h>
+
+WiFiClient wifi_client;
+PubSubClient mqtt_client(wifi_client);
+
+const char* wifi_ssid = "imd0902";
+const char* wifi_password = "imd0902iot";
+int wifi_timeout = 100000;
+
+const char* mqtt_broker = "io.adafruit.com";
+const int mqtt_port = 1883;
+int mqtt_timeout = 10000;
+
+const char* mqtt_usernameAdafruitIO = "KalvinMaia10";
+const char* mqtt_keyAdafruitIO = "aio_DoWb12tvUu7bcr9kmlSgALzYaIiQ";
+
+int valor = 0;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA); //"station mode": permite o ESP32 ser um cliente da rede WiFi
+  WiFi.begin(wifi_ssid, wifi_password);
+  connectWiFi();
+  mqtt_client.setServer(mqtt_broker, mqtt_port);
+}
+
+void loop() {
+  if (!mqtt_client.connected()) { // Se MQTT não estiver conectado
+    connectMQTT();
+  }
+
+  if (mqtt_client.connected()) {
+    valor = random(1, 100);
+     
+    mqtt_client.publish("KalvinMaia10/feeds/imd0902", String(valor).c_str());
+    Serial.println("Publicou o dado: " + String(valor));
+    delay(10000);
+    mqtt_client.loop();
+
+    //mqtt_client.publish("KalvinMaia10/feeds/imd0903", String(valor).c_str());
+    //Serial.println("Publicou o dado: " + float);
+    //delay(10000);
+    //mqtt_client.loop();
+  }
+}
+
+void connectWiFi() {
+  Serial.print("Conectando à rede WiFi .. ");
+
+  unsigned long tempoInicial = millis();
+  while (WiFi.status() != WL_CONNECTED && (millis() - tempoInicial < wifi_timeout)) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Conexão com WiFi falhou!");
+  } else {
+    Serial.print("Conectado com o IP: ");
+    Serial.println(WiFi.localIP());
+  }
+}
+
+void connectMQTT() {
+  unsigned long tempoInicial = millis();
+  while (!mqtt_client.connected() && (millis() - tempoInicial < mqtt_timeout)) {
+    if (WiFi.status() != WL_CONNECTED) {
+      connectWiFi();
+    }
+    Serial.print("Conectando ao MQTT Broker..");
+
+    if (mqtt_client.connect("ESP32Client", mqtt_usernameAdafruitIO, mqtt_keyAdafruitIO)) {
+      Serial.println();
+      Serial.print("Conectado ao broker MQTT!");
+    } else {
+      Serial.println();
+      Serial.print("Conexão com o broker MQTT falhou!");
+      delay(500);
+    }
+  }
+  Serial.println();
+}
